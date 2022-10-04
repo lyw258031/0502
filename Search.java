40ee4f94ca2ba1d7832e3fc727662a3f8b61bfd4
lyw258031 <2607984132@qq.com>
2022-10-04 17:18:31 +0800

    第一次

---
+++ Search.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Search{
+    public static void main(String []args){
+        int index,i,n,j;
+        char ch;
+        String str;
+        Scanner in=new Scanner(System.in);
+        ch=(in.nextLine()).charAt(0);
+        n=in.nextInt();
+        in.nextLine();
+        for(i=1;i<=n;i++){
+            str=in.nextLine();
+            index = -1;
+            for(j=0;j<str.length();j++){
+                if(ch == str.charAt(j)){
+                    index = j;
+                }
+            }
+            if(index>=0) System.out.println(index);
+            else System.out.println("Not Found");
+        }
+    }
+}
